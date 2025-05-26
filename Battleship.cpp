326988a5ff825db019545bdf13256dace294a295
AlCilantro <palmaolaldea@my.lanecc.edu>
2025-05-25 21:33:11 -0700

    Revised First Commit - Added description to the main file - Initial game interface

--- Battleship.cpp
+++ Battleship.cpp
@@ -3,7 +3,30 @@
 //
 
 #include "Battleship.h"
+#include <iostream>
 
 using namespace std;
 
 
+int main() {
+    cout << "       Battleship! Take out the enemy CPU to win.\n"
+    << "Game Instructions: There is a 10X10 Grid for each player.\n"
+    << "You can see your ships; and the misses or hits on the CPU's Board. "
+    << "You will be given five ships\n Carrier: 5 Squares\n Battleship: 4 Squares\n"
+    << " 2 Cruisers: 3 Squares\n Destroyer: 2 Squares\n "
+    << "A coin flip will decide who goes first." <<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
