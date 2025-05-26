301fb6eaacafab8e11553152f70d68dce50f4ea3
AlCilantro <palmaolaldea@my.lanecc.edu>
2025-05-25 21:45:56 -0700

    Fixed Commit - Added description to the main file - Initial game interface - Class placeholders made

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,32 +1,16 @@
 //
 // Created by alexa on 5/25/2025.
 //
-
 #include "Battleship.h"
 #include <iostream>
 
 using namespace std;
-
-
 int main() {
     cout << "       Battleship! Take out the enemy CPU to win.\n"
-    << "Game Instructions: There is a 10X10 Grid for each player.\n"
-    << "You can see your ships; and the misses or hits on the CPU's Board. "
-    << "You will be given five ships\n Carrier: 5 Squares\n Battleship: 4 Squares\n"
-    << " 2 Cruisers: 3 Squares\n Destroyer: 2 Squares\n "
-    << "A coin flip will decide who goes first." <<endl;
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
+     << "Game Instructions: There is a 10X10 Grid for each player.\n"
+     << "You can see your ships; and the misses or hits on the CPU's Board. "
+     << "You will be given five ships\n Carrier: 5 Squares\n Battleship: 4 Squares\n"
+     << " 2 Cruisers: 3 Squares\n Destroyer: 2 Squares\n "
+     << "A coin flip will decide who goes first." << endl;
+    return 0;
 }
--- Battleship.h
+++ Battleship.h
@@ -1,7 +1,6 @@
 //
 // Created by alexa on 5/25/2025.
 //
-
 #ifndef BATTLESHIP_H
 #define BATTLESHIP_H
 /*
@@ -10,19 +9,16 @@
 class Board {
 
 };
-
 /*
  * To determine who goes first and who's turn it is
  */
 class turn {
 
 };
-
 /*
  * Ship placement placeholder
  */
 class Ship {
 
 };
-
 #endif //BATTLESHIP_H
